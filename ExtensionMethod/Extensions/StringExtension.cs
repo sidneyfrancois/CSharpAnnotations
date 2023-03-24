008a0cd6b65acdd1583a4ab4631ad0f51e2efe70
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-24 09:08:01 -0300

    feat: add ToDateTime method, convert string into DateTime

--- ExtensionMethod/Extensions/StringExtension.cs
+++ ExtensionMethod/Extensions/StringExtension.cs
@@ -2,6 +2,13 @@ namespace Extensions
 {
     public static class StringExtension
     {
-        
+        public static DateTime ToDateTime(this string value)
+        {
+            var data = value.Split("-");
+            return new DateTime(
+                int.Parse(data[0]),
+                int.Parse(data[1]),
+                int.Parse(data[2]));
+        }       
     }
 }
