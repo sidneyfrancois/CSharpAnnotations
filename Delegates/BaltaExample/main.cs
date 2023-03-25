b6117d22287e4b230d84bb8e673964f2481e9bed
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-25 18:18:10 -0300

    feat: add two delegates in MainProgram class

--- Delegates/BaltaExample/main.cs
+++ Delegates/BaltaExample/main.cs
@@ -2,6 +2,14 @@ namespace BaltaExample
 {
     public static class MainProgram
     {
-        
+        // can be declared inside or outside of the class
+        private delegate void OnSuccessDelegate(IEnumerable<ArticleModel> data);
+        private delegate void OnErrorDelegate(ErrorModel data);
+
+        static void main()
+        {
+            OnSuccessDelegate OnSuccessHandler = OnSucess;
+            OnErrorDelegate OnErrorHandler = OnError;
+        }
     }
 }
