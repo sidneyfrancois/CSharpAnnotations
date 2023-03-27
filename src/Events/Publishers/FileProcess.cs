4c2a17142314811e857dd5188562195ded18da82
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-26 22:27:54 -0300

    refactor: make event nullable, necessary to check if an event is null before raising it within the class

--- src/Events/Publishers/FileProcess.cs
+++ src/Events/Publishers/FileProcess.cs
@@ -6,7 +6,7 @@ namespace Events.Publishers
     public class FileProcess
     {
         // event is an encapsulated delegate, delegate Notify, event ProcessCompleted of delegate type Notify
-        public event Notify ProcessCompleted;
+        public event Notify? ProcessCompleted;
 
         public void StartProcessingFile()
         {
