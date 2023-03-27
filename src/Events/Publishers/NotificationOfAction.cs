39f54034728af684ba099cabaaffe1ba9eb3da0f
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-26 21:59:15 -0300

    feat: add simple comments about event and delegate relationship

--- src/Events/Publishers/NotificationOfAction.cs
+++ src/Events/Publishers/NotificationOfAction.cs
@@ -1,7 +1,9 @@
 namespace Events.Publishers
 {
+    // publisher class
     public class ProcessingFiles
     {
+        // event is an encapsulated delegate, delegate Notify, event ProcessCompleted of delegate type Notify
         public event Notify ProcessCompleted;
     }
 }
